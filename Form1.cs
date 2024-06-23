a48780f2f6f00236d9be8ef1f0eab1404dc35422
riya925 <60502810+riya925@users.noreply.github.com>
2024-06-23 19:02:56 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+namespace Form_2
+{
+    public partial class Form1 : Form
+    {
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            if (opd.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Selected File Name:" + opd.FileName);
+            }
+        }
+    }
+}
